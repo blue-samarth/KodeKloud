9063acb0301f8fbfd71ae40a9f93e1ab7b949f4a
Samarth <samarth38work@gmail.com>
2025-08-28 18:13:36 +0000

    terraform-last?

---
+++ Terraform/Create_Security_Group_Using_Terraform.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "xfusion-sg" {
+  name        = "xfusion-sg"
+  description = "Security group for Nautilus App Servers"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "xfusion-sg"
+    Environment = "migration"
+    Project     = "nautilus-aws-migration"
+  }
+}
