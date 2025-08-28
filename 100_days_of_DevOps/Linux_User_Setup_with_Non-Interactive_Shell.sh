e090e24e00d1c591e71a0a26e50b5f7a9a0180a8
Samarth <samarth38work@gmail.com>
2025-08-28 18:12:45 +0000

    day-2

--- 100_days_of_DevOps/Linux_User_Setup_with_Non-Interactive_Shell.sh
+++ 100_days_of_DevOps/Linux_User_Setup_with_Non-Interactive_Shell.sh
@@ -2,21 +2,21 @@
 # stapp02	172.16.238.11	stapp02.stratos.xfusioncorp.com	steve	Am3ric@	Nautilus App 2
 
 thor@jumphost ~$ ssh steve@172.16.238.11
-The authenticity of host '172.16.238.11 (172.16.238.11)' can't be established.
-ED25519 key fingerprint is SHA256:eytrzVd/OemAPtW98DrrBU/jlA930fThWC9TsKpC2TI.
-This key is not known by any other names
-Are you sure you want to continue connecting (yes/no/[fingerprint])? yes
-Warning: Permanently added '172.16.238.11' (ED25519) to the list of known hosts.
-steve@172.16.238.11's password: 
+# The authenticity of host '172.16.238.11 (172.16.238.11)' can't be established.
+# ED25519 key fingerprint is SHA256:eytrzVd/OemAPtW98DrrBU/jlA930fThWC9TsKpC2TI.
+# This key is not known by any other names
+# Are you sure you want to continue connecting (yes/no/[fingerprint])? yes
+# Warning: Permanently added '172.16.238.11' (ED25519) to the list of known hosts.
+# steve@172.16.238.11's password: 
 [steve@stapp02 ~]$ sudo useradd -s /sbin/nologin yousuf
 
-We trust you have received the usual lecture from the local System
-Administrator. It usually boils down to these three things:
+# We trust you have received the usual lecture from the local System
+# Administrator. It usually boils down to these three things:
 
-    #1) Respect the privacy of others.
-    #2) Think before you type.
-    #3) With great power comes great responsibility.
+#     #1) Respect the privacy of others.
+#     #2) Think before you type.
+#     #3) With great power comes great responsibility.
 
-[sudo] password for steve: 
+# [sudo] password for steve: 
 [steve@stapp02 ~]$ id yousuf
-uid=1002(yousuf) gid=1002(yousuf) groups=1002(yousuf)
+# uid=1002(yousuf) gid=1002(yousuf) groups=1002(yousuf)
