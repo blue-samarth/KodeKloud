a0395d77c10fde7241452508a6ef261619c91da2
Samarth <samarth38work@gmail.com>
2025-09-03 18:35:13 +0000

    day-8

--- Terraform/Create_AMI_Using_Terraform.tf
+++ Terraform/Create_AMI_Using_Terraform.tf
@@ -1,14 +1,20 @@
-data "aws_instance" "datacenter_ec2" {
-  filter {
-    name   = "tag:Name"
-    values = ["datacenter-ec2"]
+# Provision EC2 instance
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-fbc0d64ab63b63828"
+  ]
+
+  tags = {
+    Name = "datacenter-ec2"
   }
 }
 
 resource "aws_ami_from_instance" "datacenter_ec2_ami" {
   name               = "datacenter-ec2-ami"
-  source_instance_id = data.aws_instance.datacenter_ec2.id
-  
+  source_instance_id = aws_instance.ec2.id
+
   tags = {
     Name = "datacenter-ec2-ami"
   }
