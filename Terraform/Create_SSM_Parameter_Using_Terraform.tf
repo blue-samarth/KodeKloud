90b9722827ac17af7d0bcda89bd1f6e1ac6daa43
blue-samarth <samarth38work@gmail.com>
2025-11-05 05:06:29 +0530

    creating-ssm-parameter

---
+++ Terraform/Create_SSM_Parameter_Using_Terraform.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "ssm_parameter_devops" {
+  name        = "devops-ssm-parameter"
+  description = "A custom SSM parameter created using Terraform"
+  type        = "String"
+  value       = "devops-value" 
+}
