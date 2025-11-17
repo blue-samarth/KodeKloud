e1b3a22322d76b3309b81c46b95d2b0cb377de35
blue-samarth <samarth38work@gmail.com>
2025-11-17 12:12:00 +0530

    deleting-iam-groups

---
+++ Terraform/Delete_IAM_Group_Using_Terraform.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_group" "this" {
+  name = "iamgroup_anita"
+}
+
+resource "null_resource" "deleting_iam_group_anita" {
+  provisioner "local-exec" {
+    command = "aws iam delete-group --group-name ${aws_iam_group.this.name}"
+  }
+  
+}
