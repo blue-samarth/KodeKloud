de661fefcb1c69c127227a90fd439f529c133a6f
Samarth <samarth38work@gmail.com>
2025-11-19 01:42:16 +0000

    iam-role-deletion

---
+++ Terraform/Delete_IAM_Role_Using_Terraform.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "role" {
+  name = "iamrole_kirsty"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "iamrole_kirsty"
+  }
+}
+
+resource "null_resource" "delete_iam_role" {
+  provisioner "local-exec" {
+    command = "aws iam delete-role --role-name ${aws_iam_role.role.name}"
+  }
+}
