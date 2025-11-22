2246dc50a6de432aaea4bc7ea15f0fde7a21a759
blue-samarth <samarth38work@gmail.com>
2025-11-22 21:53:42 +0530

    Old-Practises

---
+++ Terraform/Copy_Data_to_S3_Using_Terraform.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "nautilus-cp-9061"
+  acl    = "private"
+
+  tags = {
+    Name        = "nautilus-cp-9061"
+  }
+}
+
+# cat /tmp/nautilus.txt 
+# Welcome to KKE Cloud Labs!
+
+resource "aws_s3_bucket_object" "my_object" {
+  bucket = aws_s3_bucket.my_bucket.id
+  key    = "nautilus.txt"
+  source = "/tmp/nautilus.txt"
+  acl    = "private"
+}
