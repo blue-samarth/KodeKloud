b3c7e6744d303a5202068814720a1268fce50d19
blue-samarth <samarth38work@gmail.com>
2025-11-23 18:42:58 +0530

    name-using-variable

---
+++ Terraform/VPC_Variable_Setup_Using_Terraform.tf
@@ -0,0 +1,15 @@
+variable "KKE_vpc" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "nautilus-vpc"
+}
+
+
+
+resource "aws_vpc" "nautilus_vpc" {
+    cidr_block = "10.0.0.0/16"
+    
+    tags = {
+        Name        = var.KKE_vpc
+    }
+}
