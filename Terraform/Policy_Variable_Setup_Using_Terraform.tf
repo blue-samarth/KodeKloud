4c2e372b5f72409a6e00e9edfba64e9638f22ae2
blue-samarth <samarth38work@gmail.com>
2025-11-25 13:11:16 +0530

    setting-up-iam-policy-via-variables

---
+++ Terraform/Policy_Variable_Setup_Using_Terraform.tf
@@ -0,0 +1,21 @@
+variable "KKE_iampolicy" {
+    description = "The name of the IAM Policy"
+    type        = string
+    default     = "iampolicy_siva"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = var.KKE_iampolicy
+  description = "IAM policy allowing EC2 read actions for Siva"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ec2:Read*"]
+        Resource = "*"
+      }
+    ]
+  })
+}
